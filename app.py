edbbcf51c5172c2f94eae42436794d26d4c5dea2
SanjayT13 <sanjaytoshniwal13@gmail.com>
2025-05-24 14:06:31 +0530

    To commit changes in UI

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 from calc_func import do_addition,do_subtraction
 from multiply import do_multiplication
+from area import calculate_area_rectangle
 
 def main() : 
     print("welcome to calculator app")
     print(""" select functions from following : \n
     1. add \n 
     2. subtract \n
-    3. Multiply""")
+    3. Multiply
+    4. area_rectangle""")
     user_input = int(input("select the function : "))
     a = int(input("Value of A :"))
     b = int(input("Value of B :"))
@@ -16,6 +18,8 @@ def main() :
         result = do_subtraction(a,b) 
     elif user_input == 3 : 
         result = do_multiplication(a,b)
+    elif user_input == 4 : 
+        result = calculate_area_rectangle(a,b)
     print("result : ", result)
     return result 
 
